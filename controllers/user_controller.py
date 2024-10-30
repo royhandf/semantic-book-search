5d38da88de61103e9135c95147675a504883a55f
royhandf <royhandaffa14@gmail.com>
2024-10-30 14:58:14 +0700

    fix: session user

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -18,8 +18,11 @@ def signin_user_function():
         # Verify if the user exists and the password matches
         if user and check_password_hash(user.password, password):
             login_user(user)  # Logs in the user
-            session['user_id'] = user.id  # Store user ID in the session
-            session['user'] = user.email  # Optionally store user email or any other info
+            session['user'] = {
+                'name': user.name,
+                'email': user.email,    
+                'id': user.id
+            }
             return user
         else:
             # Return None if authentication fails
