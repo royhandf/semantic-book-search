423720dca72d14e30676335ada5af4557854d424
royhandf <royhandaffa14@gmail.com>
2025-01-01 13:12:23 +0700

    refactor: delete unused code

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
 from flask import Flask
 from config import Config
-from extensions import db, login_manager
+from extensions import db
 from flask_migrate import Migrate
+from flask_cors import CORS 
 from routes import main
-from models.user import User
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app, supports_credentials=True) 
     register_resources(app)
     register_extensions(app)
-
+    
     return app
 
 def register_extensions(app):
     db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'main.signin' 
+    jwt = JWTManager(app)
     Migrate(app, db)
 
 def register_resources(app):
     app.register_blueprint(main)
-    
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 if __name__ == '__main__':
     app = create_app()
